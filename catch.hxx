f11188a3861751b1b8131ff17af8c59c8aaf9f99
hurr <hurr@users.noreply.github.com>
2014-01-11 05:27:15 +0100

    Update catch.hxx

--- catch.hxx
+++ catch.hxx
@@ -13,8 +13,8 @@
 #ifndef __CATCH_HPP__
 #define __CATCH_HPP__
 
-#define ABS(x) (x < 0 ? -x : x)
-#define MAX(x, y) (x > y ? x : y);
+#define ABS(x)    (x < 0 ? -x : x)
+#define MAX(x, y) (x > y ?  x : y)
 
 #include <iostream>
 #include <unistd.h>
@@ -27,23 +27,26 @@
 #ifdef __unix__
 #include <sys/ioctl.h>
 #define TERM_SIZE 0x5413
+
+int      rand(void)                __THROW;
+void     srand(unsigned int seed)  __THROW;
+long int time(long int *timer)     __THROW;
 #endif
 
 #ifdef __MINGW32__
 #include <windows.h>
-int getch();
+#include   <conio.h>
+#include    <time.h>
 #endif
 
 using namespace std;
 
-typedef vector <char> CVLine;
-typedef vector <CVLine> CMatrix;
-typedef pair   <unsigned, unsigned> CPosition;
+typedef vector   <char>             CVLine;
+typedef vector <CVLine>            CMatrix;
+
+typedef pair<unsigned, unsigned> CPosition;
 
 int system(const char *command);
-int rand(void) __THROW;
-void srand(unsigned int seed) __THROW;
-long int time(long int *timer) __THROW;
 
 /* lin/win.cxx */
 void ClearScreen();
@@ -54,8 +57,8 @@ void GetCmdInfo(CPosition &cmdinfo);
 /* ai.cxx */
 char AILevel0(CPosition &PosPlayer1, CPosition &PosPlayer2);
 char AILevel1(CPosition &PosPlayer1, CPosition &PosPlayer2);
-pair<int, int> Substract(CPosition arg1, CPosition arg2);
-int distance(CPosition &PosPlayer1, CPosition &PosPlayer2);
+int  distance(CPosition &PosPlayer1, CPosition &PosPlayer2);
+pair <int, int>   Substract(CPosition arg1, CPosition arg2);
 
 constexpr char p1keys[] = {'A', 'Z', 'E', 'Q', 'D', 'W', 'X', 'C'};
 constexpr char p2keys[] = {'R', 'T', 'Y', 'F', 'H', 'V', 'B', 'N'};
